2dfe2142deac87b81e4b91b271b88fbe10dec233
Crowdbotics <team@crowdbotics.com>
2023-06-27 20:32:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "basketball-41770",
+  url: "https://basketball-41770.botics.co",
+  api: "https://basketball-41770.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
